36d8da6c401acd8d2adecc38c29c4b83b8bdd489
adrian <adrian@a.staeves.de>
2025-05-24 18:18:41 +0200

    sloved Issue #3

--- common/remove_user.php
+++ common/remove_user.php
@@ -5,10 +5,48 @@
  */
 function remove_registration($id) {
 	global $wpdb;
-	// delete registrations
+	// get some data we need to notify the first one on the wait list
+	$event_id = $wpdb->get_var($wpdb->prepare(
+		"SELECT eventid
+		FROM " . $wpdb->prefix . "sieve_registrations
+		WHERE id = %d;", $id));
+	$event = $wpdb->get_row($wpdb->prepare(
+		"SELECT date, maxspots
+		FROM ". $wpdb->prefix. "sieve_events
+		WHERE id = %d;", $event_id));
+	$lucky_reg = $wpdb->get_row($wpdb->prepare(
+		"SELECT name, email, registrationtime, id
+		FROM ". $wpdb->prefix. "sieve_registrations
+		WHERE eventid = %d
+		ORDER BY registrationtime;", $event_id), OBJECT, $event->maxspots);
+	// delete registration
 	$wpdb->query(
 		$wpdb->prepare(
 			"DELETE FROM " . $wpdb->prefix . "sieve_registrations 
 			WHERE id = %d;", 
 		$id));
+	
+	// if needed notify the lucky reg, that got a spot
+	if ($lucky_reg !== null) {
+		$mail_content = "Hallo {name},\n Für deine Event anmeldung am {date} um {time} hast du nun einen platz bekommen. \nFalls du nicht kommen kannst, sag bitte ab, indem di auf diesen Link klickst, dann bekommt der nächste den Platz\n{cancel_link}";
+		$mail_subject = "Platz für event am {date} erhalten";
+
+		// compute hash
+		$hash_ref = hash('sha256', $lucky_reg->name . $lucky_reg->registrationtime);
+		$cancel_link = esc_url(admin_url('admin-post.php')) . '?action=sieve_cancel&sieve-id=' . $lucky_reg->id . '&sieve-key=' . $hash_ref;
+		
+		// replace {} in mail_content with values
+		$replace_pairs = [["{name}", $lucky_reg->name], 
+			["{date}", (new DateTimeImmutable($event->date))->format("j.n.y")],
+			["{time}", (new DateTimeImmutable($event->date))->format("G:i")],
+			["{cancel_link}", $cancel_link]];
+		foreach ($replace_pairs as $rp) {
+			$mail_subject = str_replace($rp[0], $rp[1], $mail_subject);
+			$mail_content = str_replace($rp[0], $rp[1], $mail_content);
+		}
+
+		// actually send the mail
+		wp_mail( $lucky_reg->email, $mail_subject, $mail_content );
+	}
+
 }
